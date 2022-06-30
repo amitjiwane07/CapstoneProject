11c33c17e9a8f6d2470dc2472cf1c2002d618ad7
azhaguvel.manikkam <azhaguvel.manikkam@collabera.com>
2022-06-30 07:25:50 +0530

    employeeregister

--- EmployeeRegisterService/src/main/java/com/example/demo/service/EmployeeService.java
+++ EmployeeRegisterService/src/main/java/com/example/demo/service/EmployeeService.java
@@ -7,13 +7,16 @@ import com.example.demo.model.Employee;
 
 public interface EmployeeService {
 
-	public List<Employee> getAllEmployee();
-	
-	public Optional<Employee> getEmployeeById(Long id);
-	
-	public Employee saveEmployee(Employee employee);
-	
-	public String deleteEmployee();
-	
-	public String deleteEmployeeById(Long id);
+    public List<Employee> getAllEmployee();
+
+    public Optional<Employee> getEmployeeById(Long id);
+
+    public Employee saveEmployee(Employee employee);
+
+    Employee updateEmployee(Long id, Employee employee);
+
+    public String deleteEmployee();
+
+    public String deleteEmployeeById(Long id);
+
 }
